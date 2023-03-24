89137bb0504702a3743110686860cc6e3e25beb6
Edwin van Holland <edwin@krijgt.info>
2023-03-24 23:23:17 +0100

    Tweaked data

--- Api/Repository.cs
+++ Api/Repository.cs
@@ -22,14 +22,14 @@ namespace Api
                         },
                         new RaceTeam {Name = "Aston Martin", Aero = 90, Engine = 90, Durability = 90, Chassis = 90,
                             Drivers = new() {
-                                new RaceDriver {Name = "Fernando Alonso", Aggressiveness = 95, Experience = 99, Pace = 05, Racecraft = 95 },
-                                new RaceDriver {Name = "lance Stroll", Aggressiveness = 95, Experience = 80, Pace = 80, Racecraft = 70 }
+                                new RaceDriver {Name = "Fernando Alonso", Aggressiveness = 90, Experience = 99, Pace = 88, Racecraft = 94 },
+                                new RaceDriver {Name = "Lance Stroll", Aggressiveness = 95, Experience = 80, Pace = 80, Racecraft = 70 }
                             }
                         },
-                        new RaceTeam {Name = "Mercedes", Aero = 80, Engine = 88, Durability = 90, Chassis = 65,
+                        new RaceTeam {Name = "Mercedes", Aero = 80, Engine = 88, Durability = 90, Chassis = 75,
                             Drivers = new() {
-                                new RaceDriver {Name = "Lewis Hamilton", Aggressiveness = 85, Experience = 95, Pace = 90, Racecraft = 94 },
-                                new RaceDriver {Name = "George Russell", Aggressiveness = 80, Experience = 80, Pace = 92, Racecraft = 92 }
+                                new RaceDriver {Name = "Lewis Hamilton", Aggressiveness = 85, Experience = 95, Pace = 86, Racecraft = 94 },
+                                new RaceDriver {Name = "George Russell", Aggressiveness = 82, Experience = 80, Pace = 92, Racecraft = 88 }
                             }
                         },
                         new RaceTeam {Name = "Ferrari", Aero = 90, Engine = 95, Durability = 80, Chassis = 85,
@@ -38,7 +38,7 @@ namespace Api
                                 new RaceDriver {Name = "Carlos Sainz", Aggressiveness = 85, Experience = 80, Pace = 84, Racecraft = 80 }
                             }
                         },
-                        new RaceTeam {Name = "McLaren", Aero = 60, Engine = 88, Durability = 70, Chassis = 55,
+                        new RaceTeam {Name = "McLaren", Aero = 70, Engine = 88, Durability = 70, Chassis = 85,
                                Drivers = new() {
                                     new RaceDriver {Name = "Lando Norris", Aggressiveness = 80, Experience = 80, Pace = 92, Racecraft = 80 },
                                     new RaceDriver {Name = "Oscar Piastri", Aggressiveness = 75, Experience = 50, Pace = 80, Racecraft = 70 }
@@ -58,7 +58,7 @@ namespace Api
                         },
                         new RaceTeam {Name = "Williams", Aero = 85, Engine = 88, Durability = 85, Chassis = 75,
                                 Drivers = new() {
-                                    new RaceDriver {Name = "Alexander Albon", Aggressiveness = 75, Experience = 70, Pace = 80, Racecraft = 75 },
+                                    new RaceDriver {Name = "Alexander Albon", Aggressiveness = 70, Experience = 73, Pace = 80, Racecraft = 75 },
                                     new RaceDriver {Name = "Logan Sargeant", Aggressiveness = 75, Experience = 50, Pace = 75, Racecraft = 60 }
                             }
                         },
@@ -68,7 +68,7 @@ namespace Api
                                     new RaceDriver {Name = "Valtteri Bottas", Aggressiveness = 65, Experience = 90, Pace = 78, Racecraft = 78 }
                             }
                         },
-                        new RaceTeam {Name = "Haas", Aero = 83, Engine = 95, Durability = 80, Chassis = 75,
+                        new RaceTeam {Name = "Haas", Aero = 78, Engine = 95, Durability = 80, Chassis = 75,
                                 Drivers = new() {
                                     new RaceDriver {Name = "Kevin Magnussen", Aggressiveness = 90, Experience = 86, Pace = 80, Racecraft = 78 },
                                     new RaceDriver {Name = "Nico Hulkenberg", Aggressiveness = 65, Experience = 85, Pace = 75, Racecraft = 76 }
--- Shared/Models/RaceDriver.cs
+++ Shared/Models/RaceDriver.cs
@@ -9,6 +9,8 @@
         public int Experience { get; set; }
         public int Aggressiveness { get; set; }
 
-        public int Rating => ((Pace + Racecraft + Experience + Aggressiveness) / 4);
+        public int Rating => (int)((Pace + Racecraft + (Experience*0.25) + (Aggressiveness * 0.5)) / 2.75);
+        public int QualifyPace => Pace;
+        public int RacePace => Rating;
     }
 }
